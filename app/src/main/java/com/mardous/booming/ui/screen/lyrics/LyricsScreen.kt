087a39062a9ed63266be60f8a2811e4d7d4f7f9b
324243234 <huate1@vsmailpro.live>
2026-07-26 14:55:36 +0800

    Update LyricsScreen.kt

--- app/src/main/java/com/mardous/booming/ui/screen/lyrics/LyricsScreen.kt
+++ app/src/main/java/com/mardous/booming/ui/screen/lyrics/LyricsScreen.kt
@@ -36,6 +36,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.DisposableEffect  //
+import androidx.compose.runtime.mutableFloatStateOf  //
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.withFrameNanos
