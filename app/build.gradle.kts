84964a26b862fac5152cf38f3ff3d2cbc057817a
324243234 <huate1@vsmailpro.live>
2026-08-02 02:53:43 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -250,8 +250,7 @@ androidComponents {
         variant.outputs.forEach { output ->
             val filter = output.filters.joinToString("-") { it.identifier }
             val abi = filter.ifEmpty { "universal" }
-            //output.outputFileName = "BoomingMusic-${output.versionName.get()}-${variant.flavorName}-$abi.apk"
-			output.outputFileName = "汽水音乐-${output.versionName.get()}-${variant.flavorName}-$abi.apk"
+            output.outputFileName = "汽水音乐-${output.versionName.get()}-${variant.flavorName}-$abi.apk"
         }
     }
 }
