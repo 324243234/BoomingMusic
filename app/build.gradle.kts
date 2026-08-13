0d4cca27912e8805b46120db4cade8de39ebf958
324243234 <huate1@vsmailpro.live>
2026-08-13 15:27:31 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,7 @@ val currentVersion: Version = Version.Beta(
     versionMajor = 1,
     versionMinor = 4,
     versionPatch = 0,
-    versionBuild = 4
+    versionBuild = 5
 )
 val currentVersionCode = currentVersion.code
 
@@ -94,7 +94,7 @@ android {
         //applicationId = "com.kugou.android.lite"
         
         // 🌟 同步作者更新：versionCode 升至 1400103
-        versionCode = 1400104
+        versionCode = 1400105
         versionName = currentVersion.name
         check(versionCode == currentVersionCode)
     }
