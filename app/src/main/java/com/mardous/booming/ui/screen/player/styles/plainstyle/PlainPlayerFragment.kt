4386c852a43c9928b57619da2bb2de9e91dcc8b9
324243234 <huate1@vsmailpro.live>
2026-08-24 17:45:35 +0800

    Update PlainPlayerFragment.kt

--- app/src/main/java/com/mardous/booming/ui/screen/player/styles/plainstyle/PlainPlayerFragment.kt
+++ app/src/main/java/com/mardous/booming/ui/screen/player/styles/plainstyle/PlainPlayerFragment.kt
@@ -181,7 +181,7 @@ class PlainPlayerFragment : AbsPlayerFragment(R.layout.fragment_plain_player) {
 								
 								.data(song)
                     // 🌟 性能护城河 1：强制降采样到 128x128，内存开销减少 99%，提取速度快 100 倍！
-                    .size(256) 
+                    .size(256, 256) 
                     // 🌟 性能护城河 2：关闭硬件位图！取色算法需要直接读取内存像素，硬件位图在显存里，强行读取会引发崩溃或回退机制，消耗大量性能。
                     .allowHardware(false) 
                     .build()
